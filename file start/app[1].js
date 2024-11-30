84c4dfd3bc84949b82e2c58d62f2898ee80e0f00
HarshiitaSharma <147300119+HarshiitaSharma@users.noreply.github.com>
2024-11-30 21:20:59 +0530

    Add files via upload

---
+++ file start/app[1].js
@@ -0,0 +1,10 @@
+// This class will represent the music visualizer as a whole, similar to the
+// role that the `App` class played.
+//
+// See labwork 7 writeup for more hints and details.
+class App {
+  constructor() {
+    // TODO(you): Implement the constructor and add fields as necessary.
+  }
+  // TODO(you): Add methods as necessary.
+}
